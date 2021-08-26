13054e3eac254d1eb7195c33c26cc31795f9f969
Demarcily <60388907+Demarcily@users.noreply.github.com>
2021-08-26 13:39:47 +0200

    Update BouncingBall.java

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -1,8 +1,14 @@
 public class BouncingBall {
   public static void main(String [] args) {
-    double height = 10.1;
-    System.out.println(height);
-    //test
+    double height = 1;
+    double distance = 0;
+    int time = 0;
+    double accel;
+    double velocity;
+
+
+
+
 
 
   }
