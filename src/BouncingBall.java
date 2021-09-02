6ee6d017975ea9aaf324fcc1b01ac4560bf4aae6
Demarcily <60388907+Demarcily@users.noreply.github.com>
2021-09-02 13:22:27 +0200

    trying to understand the formula
    trying to understand the damned V1 = V0 + at formula

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -1,17 +1,31 @@
 public class BouncingBall {
   public static void main(String [] args) {
-    double height = 1;
+    double height = 1000;
+    double currentheight;
     double distance = 0;
-    int time = 0;
-    double accel;
-    double velocity;
+    int t = 0; //Time
+    double velocity = 0;
+    double newvelocity;
+    double gravity = 9.8;
 
 
 
 
+    while(t < 14) {
+      newvelocity = velocity + (gravity * t);
+      currentheight = height - newvelocity;
+      velocity = newvelocity;
+      t++;
+      System.out.println(currentheight);
+    }
+  }
 
+  private void Update() {
 
   }
 
+  private void posUpdate() {
+
+  }
 
 }
